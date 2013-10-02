abd0b6e5dd817cb936b5050279c184e294a3df76
Yi Qiao <yi.qiao.2@bc.edu>
2013-10-01 21:35:23 -0400

    Added lightLocation field to TKRayTraceScene, as well as a method for calculating the light vector at any point in the scene

--- src/teamk/hw4/controller/TKRayTraceScene.java
+++ src/teamk/hw4/controller/TKRayTraceScene.java
@@ -1,6 +1,9 @@
 package teamk.hw4.controller;
 
 import javax.media.opengl.GL2;
+import teamk.hw4.model.*;
+import teamk.hw4.utils.math.*;
+
 
 /**
  * A concrete TKScene class that draws the specific scene for HW4
@@ -9,6 +12,8 @@ import javax.media.opengl.GL2;
  *
  */
 public class TKRayTraceScene extends TKScene {
+	
+	private TKVector3 lightLocation; /**< The location of the light source */
 
 	@Override
 	public void render(GL2 gl) {
@@ -20,6 +25,18 @@ public class TKRayTraceScene extends TKScene {
 	public void updateAnimation(long timeElapsed) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	/**
+	 * Calculates the light vector that starts from a given point, and points to the light source
+	 * 
+	 * This function is not part of TKITraceable because they are the same for all objects
+	 * 
+	 * @param p   The point at which the light vector starts
+	 * @return    The light vector
+	 */
+	public TKVector3 lightVectorForObjectAtPoint(TKVector3 p) {
+		return lightLocation.sub(p).getNormalized();
 	}
 
 }
