d7e29ebb41b9e5e8b4bc11732a9c7a93b48e3705
Yi Qiao <yi.qiao.2@bc.edu>
2013-10-01 20:12:24 -0400

    Created a model class TKSphere that represents a traceable sphere in space, along with its test cases

---
+++ src/teamk/hw4/model/TKSphere.java
@@ -0,0 +1,83 @@
+package teamk.hw4.model;
+
+import teamk.hw4.utils.math.*;
+
+/**
+ * A Sphere that is ray traceable
+ * 
+ * @author Yi Qiao
+ *
+ */
+public class TKSphere extends TKMathematicaModel implements TKITraceable {
+	
+	private TKVector3 center;	/**< The center coordinate of the sphere */
+	private double    radius;	/**< The radius of the sphere */
+	
+	public TKSphere(TKVector3 center, double radius) {
+		this.center = center;
+		this.radius = radius;
+	}
+	
+	public TKVector3 getCenter() {return center;}
+	public double getRadius() {return radius;}
+
+	@Override
+	public double[] findRootsOnRay(TKRay aRay) {
+		
+		// Provide the constants used by the equations as local variables
+		double cx = center.getX(); double cy = center.getY(); double cz = center.getZ();
+		double x0 = aRay.getP0().getX(); double y0 = aRay.getP0().getY(); double z0 = aRay.getP0().getZ();
+		double x1 = aRay.getP1().getX(); double y1 = aRay.getP1().getY(); double z1 = aRay.getP1().getZ();
+		double r = radius;
+				
+		/* >>>>>> BEGIN PASTED CODE <<<<<< */
+		
+		double discriminate = 4*power((cx - x0)*(x0 - x1) + (cy - y0)*(y0 - y1) + (cz - z0)*(z0 - z1),2) -
+		           4*(power(cx,2) - power(r,2) - 2*cx*x0 + power(x0,2) + power(cy - y0,2) +
+		              power(cz - z0,2))*(power(x0 - x1,2) + power(y0 - y1,2) + power(z0 - z1,2));
+		
+		/* >>>>>> END PASTED CODE <<<<<< */
+				
+		// Test the discriminate
+		if (discriminate < 0) return null;
+
+		/* >>>>>> BEGIN PASTED CODE <<<<<< */
+
+		double t1 = (power(x0,2) + cx*x1 - x0*(cx + x1) - cy*y0 + power(y0,2) + cy*y1 -
+		        y0*y1 - cz*z0 + power(z0,2) + Sqrt(discriminate)/2.
+		          + cz*z1 - z0*z1)/(power(x0 - x1,2) + power(y0 - y1,2) + power(z0 - z1,2));
+
+		double t2 = (power(x0,2) + cx*x1 - x0*(cx + x1) - cy*y0 + power(y0,2) + cy*y1 -
+		        y0*y1 - cz*z0 + power(z0,2) - Sqrt(discriminate)/2.
+		          + cz*z1 - z0*z1)/(power(x0 - x1,2) + power(y0 - y1,2) + power(z0 - z1,2));
+		
+		/* >>>>>> END  PASTED CODE <<<<<< */
+		
+		if (doubleEqual(discriminate, 0)) {
+			// if discriminate is 0, t1 and t2 must be the same, or something weird must have happened
+			assert(doubleEqual(t1, t2));
+			
+			double[] result = new double[1];
+			result[0] = t1;
+			return result;
+		}
+		
+		// If the execution get to this point, discriminate must be greater than 0, and two roots we have
+		double[] result = new double[2];
+		result[0] = t1 < t2 ? t1 : t2; 
+		result[1] = t1 < t2 ? t2 : t1;
+		
+		return result;
+	}
+
+	@Override
+	public TKVector3 surfaceNormalAtPoint(TKVector3 p) {
+		// Calculate the normal vector by vector arithmetics
+		TKVector3 normalVector = p.sub(center);
+		
+		// Check whether p is actually on the surface by checking the normal vector length
+		// If p is not on the surface, return null
+		return doubleEqual(normalVector.getLength(), radius) ? normalVector : null;
+	}
+
+}
---
+++ src/teamk/hw4/model/tests/TKTestSphere.java
@@ -0,0 +1,145 @@
+/**
+ * 
+ */
+package teamk.hw4.model.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import teamk.hw4.model.*;
+import teamk.hw4.utils.math.*;
+
+/**
+ * Unit test case for TKSphere
+ * 
+ * @see TKSphere
+ * @author Yi Qiao
+ *
+ */
+public class TKTestSphere {
+	
+	double _tolerance = 1e-4; /**< Float point comparison tolerance */
+
+	
+	private TKSphere sphere;
+
+
+	@Before
+	public void setUp() throws Exception {
+		sphere = new TKSphere(new TKVector3(5.0, 5.0, 0.0), 2.0);
+	}
+
+	@Test
+	public void testCreation() {
+		assertTrue("The center of the sphere needs to be at (5, 5, 0)", sphere.getCenter().equals(new TKVector3(5.0, 5.0, 0.0)));
+		assertEquals("The radius of the sphere needs to be 2", 2.0, sphere.getRadius(), _tolerance);
+	}
+	
+	@Test
+	public void testTraceIntercept() {
+		// This ray that goes through the origin and the sphere's center will intercept
+		// the sphere at two points. Math is done in x-y plane and z=0 for this test
+		TKRay interceptingRay = new TKRay(TKVector3.getZeroVector(), new TKVector3(5.0, 5.0, 0.0));
+		
+		double[] roots = sphere.findRootsOnRay(interceptingRay);
+		assertTrue("There must be two roots", roots.length==2);
+		
+		assertTrue("The first t must be smaller than the second t", roots[0] < roots[1]);
+		
+		// Math done with pencil and paper suggests that these two points are
+		// (5-sqrt(2), 5-sqrt(2), 0) and (5+sqrt(2), 5+sqrt(2), 0)
+		TKVector3 ip1 = new TKVector3(interceptingRay.getParametricEquation().evaluate(roots[0]));
+		TKVector3 ip2 = new TKVector3(interceptingRay.getParametricEquation().evaluate(roots[1]));
+				
+		assertTrue("ip1 must be (5-sqrt(2), 5-sqrt(2), 0)", ip1.equals(new TKVector3(5-Math.sqrt(2.0), 5-Math.sqrt(2.0), 0.0)));
+		assertTrue("ip2 must be (5+sqrt(2), 5+sqrt(2), 0)", ip2.equals(new TKVector3(5+Math.sqrt(2.0), 5+Math.sqrt(2.0), 0.0)));
+
+	}
+	
+	@Test
+	public void testTraceTangent() {
+		
+		// On the x-z plane and y=5, the horizontal and vertical lines bounding the shpere will
+		// tangent with the sphere. The p1 of the following rays are the tangent point
+		TKRay xzTopBoundRay = new TKRay(
+				new TKVector3(new double[]{0.0, 5.0, 2.0}), 
+				new TKVector3(new double[]{5.0, 5.0, 2.0}));
+		
+		TKRay xcBottomBoundRay = new TKRay(
+				new TKVector3(new double[]{0.0, 5.0, -2.0}), 
+				new TKVector3(new double[]{5.0, 5.0, -2.0}));
+		
+		TKRay xzLeftBoundRay = new TKRay(
+				new TKVector3(new double[]{3.0, 5.0, 1.0}), 
+				new TKVector3(new double[]{3.0, 5.0, 0.0}));
+		
+		TKRay xzRightBoundRay = new TKRay(
+				new TKVector3(new double[]{7.0, 5.0, 1.0}), 
+				new TKVector3(new double[]{7.0, 5.0, 0.0}));
+		
+		double[] roots;
+		
+		roots = sphere.findRootsOnRay(xzTopBoundRay);
+		assertNotNull("For xzTopBoundRay there must be a root", roots);
+		assertTrue("For xzTopBoundRay there must be only one root", roots.length == 1);
+		assertEquals("For xzTopBoundRay, t must equals 1", 1.0, roots[0], _tolerance);
+		
+		roots = sphere.findRootsOnRay(xcBottomBoundRay);
+		assertNotNull("For xcBottomBoundRay there must be a root", roots);
+		assertTrue("For xcBottomBoundRay there must be only one root", roots.length == 1);
+		assertEquals("For xcBottomBoundRay, t must equals 1", 1.0, roots[0], _tolerance);
+		
+		roots = sphere.findRootsOnRay(xzLeftBoundRay);
+		assertNotNull("For xzLeftBoundRay there must be a root", roots);
+		assertTrue("For xzLeftBoundRay there must be only one root", roots.length == 1);
+		assertEquals("For xzLeftBoundRay, t must equals 1", 1.0, roots[0], _tolerance);
+		
+		roots = sphere.findRootsOnRay(xzRightBoundRay);
+		assertNotNull("For xzRightBoundRay there must be a root", roots);
+		assertTrue("For xzRightBoundRay there must be only one root", roots.length == 1);
+		assertEquals("For xzRightBoundRay, t must equals 1", 1.0, roots[0], _tolerance);
+
+		// On the x-z plane and y=5, a ray that goes through (0,5,0) will tangent the sphere
+		// at (21/5, 5, 2*sqrt(21)/5)
+		TKRay xzTangentRay = new TKRay(new TKVector3(0.0, 5.0, 0.0), new TKVector3(21.0/5.0, 5.0, 2.0*Math.sqrt(21.0)/5.0));
+		roots = sphere.findRootsOnRay(xzTangentRay);
+		assertNotNull("For xzTangentRay there must be a root", roots);
+		assertTrue("For xzTangentRay there must be only one root", roots.length == 1);
+		assertEquals("For xzTangentRay, t must equals 1", 1.0, roots[0], _tolerance);
+		
+		// a ray that goes through (0,0,0) and the previously calculated point should
+		// also tangent the sphere since they are in the same plane which tangent the sphere
+		TKRay tangentRay = new TKRay(new TKVector3(0.0, 0.0, 0.0), new TKVector3(21.0/5.0, 5.0, 2.0*Math.sqrt(21.0)/5.0));
+		roots = sphere.findRootsOnRay(tangentRay);
+		assertNotNull("For tangentRay there must be a root", roots);
+		assertTrue("For tangentRay there must be only one root", roots.length == 1);
+		assertEquals("For tangentRay, t must equals 1", 1.0, roots[0], _tolerance);
+	}
+	
+	@Test
+	public void testTraceNoRoot() {
+		// Any ray in a x-z plane with y < 3 or y>7 will not intercept the sphere
+		TKRay nonInterceptingRay1 = new TKRay(new TKVector3(0.0, 2.9, 0.0), new TKVector3(10.0, 2.9, 10.0));
+		TKRay nonInterceptingRay2 = new TKRay(new TKVector3(0.0, 7.1, 0.0), new TKVector3(10.0, 7.1, 10.0));
+
+		double[] roots;
+		
+		roots = sphere.findRootsOnRay(nonInterceptingRay1);
+		assertNull("For nonInterceptingRay1 there must be no root", roots);
+		
+		roots = sphere.findRootsOnRay(nonInterceptingRay2);
+		assertNull("For nonInterceptingRay2 there must be no root", roots);
+		
+		// On the x-z plane where y=5
+		TKRay nonInterceptingRay3 = new TKRay(new TKVector3(0.0, 5.0, 0.0), new TKVector3(5.0, 5.0, 3.0));
+		TKRay nonInterceptingRay4 = new TKRay(new TKVector3(0.0, 5.0, 0.0), new TKVector3(5.0, 5.0, -3.0));
+		
+		roots = sphere.findRootsOnRay(nonInterceptingRay3);
+		assertNull("For nonInterceptingRay3 there must be no root", roots);
+		
+		roots = sphere.findRootsOnRay(nonInterceptingRay4);
+		assertNull("For nonInterceptingRay4 there must be no root", roots);
+	}
+
+}
