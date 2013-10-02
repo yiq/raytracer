78cd5886a432414afb25417499a656e17c5d3cd8
Yi Qiao <yi.qiao.2@bc.edu>
2013-10-01 21:27:46 -0400

    Better comments in TKITraceable

--- src/teamk/hw4/model/TKITraceable.java
+++ src/teamk/hw4/model/TKITraceable.java
@@ -9,8 +9,19 @@ import teamk.hw4.utils.math.*;
  */
 public abstract interface TKITraceable {
 	
+	/**
+	 * Return a set of points that are both on the traceable object and the ray
+	 * 
+	 * @param aRay The ray used in ray tracing
+	 * @return A set of parameter t for the traced out points. length may vary. null if not intercepting
+	 */
 	abstract public double[] findRootsOnRay(TKRay aRay);
 	
+	/**
+	 * Returns the surface normal at point p
+	 * @param p A point on the surface
+	 * @return	The surface normal at the given point. null if the given point is not actually on the surface
+	 */
 	abstract public TKVector3 surfaceNormalAtPoint(TKVector3 p);
 
 }
