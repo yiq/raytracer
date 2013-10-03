4027b3ae835a8ecd3cdb7742e072812d51f4b4da
Yi Qiao <yi.qiao.2@bc.edu>
2013-10-03 18:35:51 -0400

    Changed TKSphereLatLongUVMapper to TKSphereLongLatUVMapper since most texture image are mapped with the Longitude as the x axis

--- src/teamk/hw4/model/uvmapper/tests/TKTestSphereLongLatUVMapper.java
+++ src/teamk/hw4/model/uvmapper/tests/TKTestSphereLongLatUVMapper.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 import teamk.hw4.model.uvmapper.*;
 import teamk.hw4.utils.math.TKVector3;
 
-public class TKTestSphereLatLongUVMapper {
+public class TKTestSphereLongLatUVMapper {
 	
 	double _tolerance = 1e-4; /**< Float point comparison tolerance */
 
@@ -19,8 +19,8 @@ public class TKTestSphereLatLongUVMapper {
 	@Before
 	public void setUp() throws Exception {
 		
-		centeredSphereMapper = new TKSphereLatLongUVMapper(new TKVector3(0.0, 0.0, 0.0), 1.0);
-		offsettedSphereMapper = new TKSphereLatLongUVMapper(new TKVector3(1.0, 1.0, 0.0), 2.0);
+		centeredSphereMapper = new TKSphereLongLatUVMapper(new TKVector3(0.0, 0.0, 0.0), 1.0);
+		offsettedSphereMapper = new TKSphereLongLatUVMapper(new TKVector3(1.0, 1.0, 0.0), 2.0);
 	}
 
 	@Test
@@ -36,24 +36,24 @@ public class TKTestSphereLatLongUVMapper {
 		
 		p = new TKVector3(0.0, 0.0, 1.0);
 		latLong = centeredSphereMapper.mapPointToUV(p);
-		assertEquals("(0,0,1) should be mapped to Lat = 0", 0.0, latLong[0], _tolerance);
-		assertEquals("(0,0,1) should be mapped to Long = 0", 0.0, latLong[1], _tolerance);
+		assertEquals("(0,0,1) should be mapped to Lat = 0", 0.0, latLong[1], _tolerance);
+		assertEquals("(0,0,1) should be mapped to Long = 0", 0.0, latLong[0], _tolerance);
 
 		
 		p = new TKVector3(1.0, 0.0, 0.0);
 		latLong = centeredSphereMapper.mapPointToUV(p);
-		assertEquals("(1,0,0) should be mapped to Lat = 0", 0.0, latLong[0], _tolerance);
-		assertEquals("(1,0,0) should be mapped to Long = 90", 90.0, latLong[1], _tolerance);
+		assertEquals("(1,0,0) should be mapped to Lat = 0", 0.0, latLong[1], _tolerance);
+		assertEquals("(1,0,0) should be mapped to Long = 90", 90.0, latLong[0], _tolerance);
 
 		p = new TKVector3(0.0, 0.0, -1.0);
 		latLong = centeredSphereMapper.mapPointToUV(p);
-		assertEquals("(0,0,-1) should be mapped to Lat = 0", 0.0, latLong[0], _tolerance);
-		assertEquals("(0,0,-1) should be mapped to Long = 180", 180.0, latLong[1], _tolerance);
+		assertEquals("(0,0,-1) should be mapped to Lat = 0", 0.0, latLong[1], _tolerance);
+		assertEquals("(0,0,-1) should be mapped to Long = 180", 180.0, latLong[0], _tolerance);
 		
 		p = new TKVector3(-1.0, 0.0, 0.0);
 		latLong = centeredSphereMapper.mapPointToUV(p);
-		assertEquals("(-1,0,0) should be mapped to Lat = 0", 0.0, latLong[0], _tolerance);
-		assertEquals("(-1,0,0) should be mapped to Long = 270", 270.0, latLong[1], _tolerance);
+		assertEquals("(-1,0,0) should be mapped to Lat = 0", 0.0, latLong[1], _tolerance);
+		assertEquals("(-1,0,0) should be mapped to Long = 270", 270.0, latLong[0], _tolerance);
 		
 		
 		for(int i=0; i<360; i++) {
@@ -61,20 +61,20 @@ public class TKTestSphereLatLongUVMapper {
 			double z = Math.cos(Math.toRadians(i));
 			p = new TKVector3(x, 0.0, z);
 			latLong = centeredSphereMapper.mapPointToUV(p);
-			assertEquals("circle on y=0 should be mapped to Lat = 0", 0.0, latLong[0], _tolerance);
-			assertEquals("circle on y=0 should be mapped to i", i, latLong[1], _tolerance);
+			assertEquals("circle on y=0 should be mapped to Lat = 0", 0.0, latLong[1], _tolerance);
+			assertEquals("circle on y=0 should be mapped to i", i, latLong[0], _tolerance);
 		}
 		
 		// Latitude
 		p = new TKVector3(0.0, 1.0, 0.0);
 		latLong = centeredSphereMapper.mapPointToUV(p);
-		assertEquals("(0,1,0) should be mapped to Lat = 90", 90.0, latLong[0], _tolerance);
-		assertEquals("(0,1,0) should be mapped to Long = 0", 0.0, latLong[1], _tolerance);
+		assertEquals("(0,1,0) should be mapped to Lat = 90", 90.0, latLong[1], _tolerance);
+		assertEquals("(0,1,0) should be mapped to Long = 0", 0.0, latLong[0], _tolerance);
 		
 		p = new TKVector3(0.0, -1.0, 0.0);
 		latLong = centeredSphereMapper.mapPointToUV(p);
-		assertEquals("(0,-1,0) should be mapped to Lat = -90", -90.0, latLong[0], _tolerance);
-		assertEquals("(0,-1,0) should be mapped to Long = 0", 0.0, latLong[1], _tolerance);
+		assertEquals("(0,-1,0) should be mapped to Lat = -90", -90.0, latLong[1], _tolerance);
+		assertEquals("(0,-1,0) should be mapped to Long = 0", 0.0, latLong[0], _tolerance);
 		
 	}
 	
@@ -88,20 +88,20 @@ public class TKTestSphereLatLongUVMapper {
 			double z = 2.0 * Math.cos(Math.toRadians(i));
 			p = new TKVector3(x, 1.0, z);
 			latLong = offsettedSphereMapper.mapPointToUV(p);
-			assertEquals("circle on y=0 should be mapped to Lat = 0", 0.0, latLong[0], _tolerance);
-			assertEquals("circle on y=0 should be mapped to i", i, latLong[1], _tolerance);
+			assertEquals("circle on y=0 should be mapped to Lat = 0", 0.0, latLong[1], _tolerance);
+			assertEquals("circle on y=0 should be mapped to i", i, latLong[0], _tolerance);
 		}
 		
 		// Latitude
 		p = new TKVector3(1.0, 3.0, 0.0);
 		latLong = offsettedSphereMapper.mapPointToUV(p);
-		assertEquals("(1,3,0) should be mapped to Lat = 90", 90.0, latLong[0], _tolerance);
-		assertEquals("(1,3,0) should be mapped to Long = 0", 0.0, latLong[1], _tolerance);
+		assertEquals("(1,3,0) should be mapped to Lat = 90", 90.0, latLong[1], _tolerance);
+		assertEquals("(1,3,0) should be mapped to Long = 0", 0.0, latLong[0], _tolerance);
 		
 		p = new TKVector3(1.0, -1.0, 0.0);
 		latLong = offsettedSphereMapper.mapPointToUV(p);
-		assertEquals("(1,-1,0) should be mapped to Lat = -90", -90.0, latLong[0], _tolerance);
-		assertEquals("(1,-1,0) should be mapped to Long = 0", 0.0, latLong[1], _tolerance);
+		assertEquals("(1,-1,0) should be mapped to Lat = -90", -90.0, latLong[1], _tolerance);
+		assertEquals("(1,-1,0) should be mapped to Long = 0", 0.0, latLong[0], _tolerance);
 		
 	}
 
